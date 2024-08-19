869c7c37fe8cad03cc8f4ffc3ad8a6b092d4802e
Ethan <Ethanlukandwa@gmail.com>
2024-08-18 21:30:25 -0500

    Finished the INSERT querying method. Adds the user selected into the database

--- login.php
+++ login.php
@@ -1,10 +1,11 @@
 <?php
 include('connection.php');
 //if the submit button was clicked then it will start the query
+$database = mysqli_connect('localhost', 'root', "", 'mymotiv');
 if (isset($_POST['submit-button'])) {
     //getting the values from the username input and the password input
-    $username = mysqli_real_escape_string($database, $_POST['login-user']);
-    $password = mysqli_real_escape_string($database, $_POST['login-pwd']);
+    $username = $_POST['login-username'];
+    $password = $_POST['login-pwd'];
     $errors = array();
 
     //making sure that they are properly filled
@@ -12,23 +13,42 @@ if (isset($_POST['submit-button'])) {
         array_push($errors, "Username Required");
     }
     if (empty($password)) {
-        
+
         array_push($errors, "Password required");
     }
 
+
+
     //checking if username already exists in the database
-    $user_query = "SELECT * FROM usersTest WHERE username='$username'";
-    $result_of_check = mysqli_query($database, $user_query);
+    $user_query = "SELECT * FROM usersTest WHERE username ='$username'";
+    $result_of_check = mysqli_query($conn, $user_query);
     $user = mysqli_fetch_assoc($result_of_check);
-
-    if ($user['username'] === $username) {
-        array_push($errors, "Username already exists");
+    if ($user) {
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+            echo "Username already exists";
+            
+        }
     }
 
-    if(count($errors) == 0){
-        $query = "INSERT INTO userTest (username, password) VALUES ('$username', '$password')";
-        
-    }
 
-    
+    if (count($errors) == 0) {
+        $query = "INSERT INTO usersTest (username, password) VALUES ('$username', '$password')";
+        $result = mysqli_query($conn, $query);
+
+        if ($result == false) {
+            echo "Error execution query: " . mysqli_error($conn);
+        } else {
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $count = mysqli_num_rows($result);
+            if ($count == 1) {
+                header("Location:welcome.php");
+            } else {
+                echo "<script>
+                window.location.href = 'index.php'
+                alert('Login failed. Insertion didnt work')
+                </script>";
+            }
+        }
+    }
 }
