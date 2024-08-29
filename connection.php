095ce4ca6997ea45b03f74d6168f71912150a165
Ethan <Ethanlukandwa@gmail.com>
2024-08-29 13:26:39 -0500

    Securing credentials for SMTP and Database.

--- connection.php
+++ connection.php
@@ -1,11 +1,13 @@
 <?php
+$env = parse_ini_file('.env');
 //file to connect the website to my database
-$servername = "localhost";
-$username = "root";
-$db = "mymotiv";
+$servername = $env['DB_HOST'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASSWORD'];
+$db = $env['DB_NAME'];
 
 
-$conn = new mysqli($servername, $username, "", $db);
+$conn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed" . $conn->connect_error);
 }
