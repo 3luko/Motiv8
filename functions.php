e9832092312a3e1cc95972dd8262ceefb5d6d151
Ethan <Ethanlukandwa@gmail.com>
2024-09-01 23:59:25 -0500

    Finished initial sign-up for first time users.

--- functions.php
+++ functions.php
@@ -9,35 +9,37 @@ $env = parse_ini_file('.env');
 
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);
+function send_Entry_Mail($email){
+    $mail = new PHPMailer(true);
+    $env = parse_ini_file('.env');
 
-// function send_Entry_Mail($email){
-//     $mail = new PHPMailer(true);
-//     $env = parse_ini_file('.env');
-
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = $env['SMTP_USER'];                      //SMTP username
+        $mail->Password   = $env['SMTP_PASSWORD'];                  //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
+        //Recipients
+        $mail->setFrom('noreplymotiv8@gmail.com', 'Mailer');
+        $mail->addAddress($email, 'Ethan Lukoooo');     //Add a recipient
+        $mail->addReplyTo('noreplymotiv8@gmail.com', '3luko');      //Name is optional
 
-//     try {
-//         //Server settings
-//         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-//         $mail->isSMTP();                                            //Send using SMTP
-//         $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
-//         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-//         $mail->Username   = $env['SMTP_USER'];              //SMTP username
-//         $mail->Password   = $env['SMTP_PASSWORD'];                     //SMTP password
-//         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-//         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    
-//         //Recipients
-//         $mail->setFrom('noreplymotiv8@gmail.com', 'Mailer');
-//         $mail->addAddress('ethanlukandwa@gmail.com', 'Ethan Lukoooo');     //Add a recipient
-//         $mail->addReplyTo('noreplymotiv8@gmail.com', '3luko');      //Name is optional
-        
-//     } catch (Exception $e) {
-//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-//     }
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Welcome to Motiv8!';
+        $mail->Body    = 'This will be your first quote: Try your best in everything you do. Your time will come.';
 
-// }
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+}
 
 function addUser($input){
 
